11c59421d0c9e67b71e1edc1626efd82824bb01f
Eric <ericsousa@ciandt.com>
2022-08-19 16:06:08 -0300

    [Feature] Primeiro commit

---
+++ src/main/java/br/com/desafio/totalshake/TotalshakeApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.totalshake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalshakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalshakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/totalshake/TotalshakeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.totalshake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalshakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
