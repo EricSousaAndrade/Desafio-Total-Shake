1929f1fe5ee51c6455a12c634b2a439b7fc96496
Lucas <lucasst@ciandt.com>
2022-09-02 11:47:18 -0300

    :heavy_plus_sign: build - Adicionando documentação automática Open Api

--- Desafio-Total-Shake/src/main/java/br/com/desafio/totalshake/application/controller/PedidoController.java
+++ Desafio-Total-Shake/src/main/java/br/com/desafio/totalshake/application/controller/PedidoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/pedidos")
